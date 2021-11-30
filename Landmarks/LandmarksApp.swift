7cdeddb59ea91f1a3740696da3a38dcb08a99018
Vadim Rufov <vadim.rufov@gs-labs.ru>
2021-11-30 22:08:06 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Вадим Руфов on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
