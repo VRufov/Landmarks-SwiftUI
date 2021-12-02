dfcf0d72a5e9acc820f8e04d22666274148361b0
Vadim Rufov <vadim.rufov@gs-labs.ru>
2021-12-02 16:55:18 +0300

    Add dynamically filled list of landmarks

---
+++ Landmarks/Model/Coordinates.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinates.swift
+//  Landmarks
+//
+//  Created by Вадим Руфов on 01.12.2021.
+//
+
+import Foundation
+
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  Landmarks.swift
+//  Landmarks
+//
+//  Created by Вадим Руфов on 02.12.2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
